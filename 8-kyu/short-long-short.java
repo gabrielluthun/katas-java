dd701b5f4fb7ef9b14897a2d2e80ef78ea3cf356
Gabriel Luthun <gabriel.luthun@gmail.com>
2024-12-05 16:44:51 +0100

    docs(8-kyu): add 1 kata

---
+++ 8-kyu/short-long-short.java
@@ -0,0 +1,9 @@
+class ShortLongShort {
+    public static String solution(String a, String b) {
+        if (a.length() < b.length()) {
+            return a + b + a;
+        } else {
+            return b + a + b;
+        }
+    }
+}
